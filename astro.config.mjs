23f67378075ca23f0826891d0c94e40e5b73f0d9
Lucas Sempe <lucas.sempe@gmail.com>
2026-02-20 21:49:51 +0000

    Update site URL to sempe.dev

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,7 @@ import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://lsempe77.github.io',
+  site: 'https://sempe.dev',
   vite: {
     plugins: [tailwindcss()]
   }
